017bb6bf4f50db65695e03e025d9c74d27970f34
Parsa Noori <parsanoori01@yahoo.com>
2020-11-22 15:42:21 +0330

    Up to change function

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,2 +1,63 @@
 #include "LinkedList.h"
+/***
+ * Due to ease of typing and reading the
+ * comments I alias TC as Time Complexity
+ */
 
+struct LinkedList::Node** LinkedList::getAddressOfPtr(int index){
+	if (index >= size)
+		throw std::out_of_range("Index is bigger than size");
+
+	struct Node** pp = &first;
+
+	// iteration happens by the amount index which takes O(n)
+	for (int i = 0; i < index; i++)
+		pp = &(*pp)->next;
+
+	return pp;
+}
+
+/*
+ * Since Other parts of the function than
+ * the loop have the time complexity of 
+ * O(1) the function has the time complexity
+ * of O(n) 
+*/
+void LinkedList::add(int x)
+{
+	struct Node** pp = &first;
+	while (*pp) // iterating through all nodes needs time complexity of O(n)
+		pp = &(*pp)->next;
+	*pp = new Node { x, nullptr };
+	size++;
+}
+
+/**
+ * Other than iteration all works are in
+ * TC of O(n) hence the function takes TC
+ * of O(n).
+ */
+bool LinkedList::remove(int index)
+{
+	if (index >= size)
+		return false;
+
+	struct Node** pp = &first;
+
+	// iteration happens by the amount index which takes O(n)
+	for (int i = 0; i < index; i++)
+		pp = &(*pp)->next;
+
+	(*pp) = (*pp)->next;
+	return true;
+}
+
+int LinkedList::get(int index)
+{
+	if (index >= size)
+		return false;
+
+	struct Node** pp = &first;
+
+	for
+}
--- LinkedList.h
+++ LinkedList.h
@@ -1,9 +1,26 @@
-#ifndef LinkedList
-#define LinkedList
+#ifndef LinkedListClass
+#define LinkedListClass
+
+#include<stdexcept>
 
 class LinkedList {
-    private:
-	Node first;
+	struct Node {
+		int x;
+		struct Node* next;
+	};
+
+	struct Node* first = nullptr;
+	int size = 0;
+
+	struct Node** getAddressOfPtr(int index); 
+
+    public:
+
+	void add(int x);
+	void remove(int index);
+	int get(int index);
+	int search(int x);
+	void change(int index, int x);
 };
 
 #endif
